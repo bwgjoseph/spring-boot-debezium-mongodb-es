d93f2a0c72b063f8167744ed080433d4cc787f78
bwgjoseph <josephgan@live.com.sg>
2023-06-11 11:26:30 +0800

    use fixed offset file than always create one in temp directory

--- src/main/java/com/bwgjoseph/springbootdebeziummongodbes/debezium/DebeziumConnectorConfig.java
+++ src/main/java/com/bwgjoseph/springbootdebeziummongodbes/debezium/DebeziumConnectorConfig.java
@@ -1,8 +1,8 @@
 package com.bwgjoseph.springbootdebeziummongodbes.debezium;
 
-import java.io.File;
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import io.debezium.config.Configuration;
@@ -10,23 +10,25 @@ import io.debezium.config.Configuration;
 @org.springframework.context.annotation.Configuration
 public class DebeziumConnectorConfig {
     // based off https://debezium.io/documentation/reference/2.1/connectors/mongodb.html#mongodb-connector-properties
+
+    @Value("${dbz.offset.file}")
+    public String storageFile;
+
     @Bean
     public Configuration mongodbConnector() throws IOException {
-        File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
-
         return Configuration.create()
                 // engine properties
                 .with("name", "sbd-mongodb-cdc")
                 .with("connector.class", "io.debezium.connector.mongodb.MongoDbConnector")
                 .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
+                .with("offset.storage.file.filename", storageFile)
                 .with("offset.flush.interval.ms", "60000")
                 // connector specific properties
-                // .with("mongodb.hosts", "sb-debezium.1ewsyzd.mongodb.net")
                 .with("mongodb.connection.string", "mongodb://root:password@localhost:27017/source?directConnection=true&authSource=admin")
                 .with("topic.prefix", "dbz")
-                .with("mongodb.user", "root")
-                .with("mongodb.password", "password")
+                // .with("mongodb.hosts", "sb-debezium.1ewsyzd.mongodb.net")
+                // .with("mongodb.user", "root")
+                // .with("mongodb.password", "password")
                 // .with("mongodb.authsource", "admin") // has default
                 .with("mongodb.ssl.enabled", "false") // default false
                 // .with("mongodb.ssl.invalid.hostname.allowed", "false") // has default
