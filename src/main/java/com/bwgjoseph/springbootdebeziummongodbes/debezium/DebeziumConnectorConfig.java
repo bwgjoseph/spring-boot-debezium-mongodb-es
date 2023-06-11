d02aab1fb7ed5b5481ab4ddadc669acb6e06ea16
bwgjoseph <josephgan@live.com.sg>
2023-06-11 20:15:55 +0800

    change to use io.debezium.config to define the property field
    this allow for typesafe, remove the possibility of typo

--- src/main/java/com/bwgjoseph/springbootdebeziummongodbes/debezium/DebeziumConnectorConfig.java
+++ src/main/java/com/bwgjoseph/springbootdebeziummongodbes/debezium/DebeziumConnectorConfig.java
@@ -1,54 +1,53 @@
 package com.bwgjoseph.springbootdebeziummongodbes.debezium;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
+import io.debezium.config.CommonConnectorConfig;
+import io.debezium.config.CommonConnectorConfig.EventProcessingFailureHandlingMode;
 import io.debezium.config.Configuration;
+import io.debezium.connector.mongodb.MongoDbConnectorConfig;
+import io.debezium.embedded.EmbeddedEngine;
 
 @org.springframework.context.annotation.Configuration
 public class DebeziumConnectorConfig {
-    // based off https://debezium.io/documentation/reference/2.1/connectors/mongodb.html#mongodb-connector-properties
+    // based off https://debezium.io/documentation/reference/2.2/connectors/mongodb.html#mongodb-connector-properties
 
     @Value("${dbz.offset.file}")
     public String storageFile;
 
     @Bean
-    public Configuration mongodbConnector() throws IOException {
+    public Configuration mongodbConnector() {
         return Configuration.create()
                 // engine properties
-                .with("name", "sbd-mongodb-cdc")
-                .with("connector.class", "io.debezium.connector.mongodb.MongoDbConnector")
-                .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-                .with("offset.storage.file.filename", storageFile)
-                .with("offset.flush.interval.ms", "60000")
+                .with(EmbeddedEngine.ENGINE_NAME, "sbd-mongodb-cdc")
+                .with(EmbeddedEngine.CONNECTOR_CLASS, "io.debezium.connector.mongodb.MongoDbConnector")
+                .with(EmbeddedEngine.OFFSET_STORAGE, "org.apache.kafka.connect.storage.FileOffsetBackingStore")
+                .with(EmbeddedEngine.OFFSET_STORAGE_FILE_FILENAME, storageFile)
+                .with(EmbeddedEngine.OFFSET_FLUSH_INTERVAL_MS, "60000")
                 // connector specific properties
-                .with("mongodb.connection.string", "mongodb://root:password@localhost:27017/source?directConnection=true&authSource=admin")
-                .with("topic.prefix", "dbz")
-                // .with("mongodb.hosts", "sb-debezium.1ewsyzd.mongodb.net")
-                // .with("mongodb.user", "root")
-                // .with("mongodb.password", "password")
-                // .with("mongodb.authsource", "admin") // has default
-                .with("mongodb.ssl.enabled", "false") // default false
-                // .with("mongodb.ssl.invalid.hostname.allowed", "false") // has default
-                .with("database.include.list", "source") // default empty
-                // .with("database.exclude.list", "") // has default
-                // .with("collection.include.list", "") // has default
-                // .with("collection.exclude.list", "") // has default
-                // .with("snapshot.mode", "initial") // has default
-                .with("capture.mode", "change_streams_update_full_with_pre_image") // has default
-                // .with("snapshot.include.collection.list", "") // default collection.include.list
-                // .with("field.exclude.list", "") // has default
-                // .with("field.renames", "") // has default
-                // .with("tasks.max", "1") // has default
-                // .with("snapshot.max.threads", "1") // has default
-                // .with("tombstones.on.delete", "true") // has default
-                .with("snapshot.delay.ms", "100")
-                // .with("snapshot.fetch.size", "0") // has default
-                // .with("schema.name.adjustment.mode", "none") // has default
-                // debug
-                .with("errors.log.include.messages", "true")
+                .with(CommonConnectorConfig.TOPIC_PREFIX, "dbz")
+                .with(CommonConnectorConfig.SNAPSHOT_DELAY_MS, "100")
+                // .with(CommonConnectorConfig.SNAPSHOT_FETCH_SIZE, "0") // has default
+                .with(CommonConnectorConfig.SCHEMA_NAME_ADJUSTMENT_MODE, "none") // has default
+                .with(CommonConnectorConfig.EVENT_PROCESSING_FAILURE_HANDLING_MODE, EventProcessingFailureHandlingMode.FAIL)
+                // mongo connector specific properties
+                .with(MongoDbConnectorConfig.CONNECTION_STRING, "mongodb://root:password@localhost:27017/source?directConnection=true&authSource=admin")
+                // .with(MongoDbConnectorConfig.USER, "root")
+                // .with(MongoDbConnectorConfig.PASSWORD, "password")
+                // .with(MongoDbConnectorConfig.AUTH_SOURCE, "admin") // has default
+                .with(MongoDbConnectorConfig.SSL_ENABLED, "false") // default false
+                // .with(MongoDbConnectorConfig.SSL_ALLOW_INVALID_HOSTNAMES, "false") // has default
+                .with(MongoDbConnectorConfig.DATABASE_INCLUDE_LIST, "source") // default empty
+                // .with(MongoDbConnectorConfig.DATABASE_EXCLUDE_LIST, "") // has default
+                // .with(MongoDbConnectorConfig.COLLECTION_INCLUDE_LIST, "") // has default
+                // .with(MongoDbConnectorConfig.COLLECTION_EXCLUDE_LIST, "") // has default
+                // .with(MongoDbConnectorConfig.SNAPSHOT_MODE, "initial") // has default
+                .with(MongoDbConnectorConfig.CAPTURE_MODE, "change_streams_update_full_with_pre_image") // has default
+                // .with(MongoDbConnectorConfig.FIELD_EXCLUDE_LIST, "") // has default
+                // .with(MongoDbConnectorConfig.FIELD_RENAMES, "") // has default
+                // .with(MongoDbConnectorConfig.SNAPSHOT_MAX_THREADS, "1") // has default
+                // .with(MongoDbConnectorConfig.TOMBSTONES_ON_DELETE, "true") // has default
                 .build();
     }
 }
